584067016713d0269df8a1c646a28a0ee5711866
Tushar K <ktusharhk@gmail.com>
2021-05-24 01:01:00 +1000

    no change; basic code re arrangement

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -2,18 +2,14 @@ import { Injectable } from '@angular/core';
 import { Post } from './post.model';
 import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 // {providedIn: 'root'} ensures there is a single instance of PostService i.e. Singleton pattern
 @Injectable({ providedIn: 'root' })
 export class PostService {
     private posts: Post[] = [];
-    private postCount = 0;
-    private postData: {
-        postCount: number,
-        posts: Post[]
-    }
+
     private postsUpdated = new Subject<{ posts: Post[], postCount: number }>();
 
     constructor(private http: HttpClient, private router: Router) { }
@@ -52,6 +48,42 @@ export class PostService {
     getPost(id: string) {
         //return { ...this.posts.find(p => p.id === postId) }
         return this.http.get<{ _id: string, title: string, content: string, imagePath: string }>('http://localhost:3000/api/posts/' + id);
+    }
+
+    addPost(title: string, content: string, image: File) {
+        // ** ImpNote: Using formData instead of Post JSON object as we have the image as a File object.
+        // and json can't include a file.
+        // @See Lecture 80 --> 0:22 timestamp
+        const postData = new FormData();
+        postData.append("title", title);
+        postData.append("content", content);
+        postData.append('image', image, title);
+        this.http.post<{ message: string, post: Post }>('http://localhost:3000/api/posts', postData)
+            // Alternate way to send the transformedPost with an id to .subscriber().    
+            // .pipe(map((postMongData) => {
+            //     console.log(postMongData.message);
+            //     const post = postMongData.post;
+            //     return {
+            //         title: post.title,
+            //         content: post.content,
+            //         id: post._id
+            //     }
+            // }))
+            .subscribe((responseData) => {
+                //** Imp Note: The below code to emit change post event is commented off since we navigate 
+                // back to the root where we reload the posts by calling the getPosts() method from the ngOnInit() method of the post-list.component */
+
+                //console.log(postMongData.message);
+                // const post: Post = {
+                //     id: responseData.post.id,
+                //     title: title,
+                //     content: content,
+                //     imagePath: responseData.post.imagePath
+                // }
+                // this.posts.push(post);
+                // this.postsUpdated.next([...this.posts]);
+                this.router.navigate(["/"]);
+            });
     }
 
     // The image could either be a File object if freshly loaded from local storage
@@ -93,43 +125,6 @@ export class PostService {
                 // this.postsUpdated.next([...this.posts]);
                 this.router.navigate(["/"]);
             });
-    }
-
-    addPost(title: string, content: string, image: File) {
-        // ** ImpNote: Using formData instead of Post JSON object as we have the image as a File object.
-        // and json can't include a file.
-        // @See Lecture 80 --> 0:22 timestamp
-        const postData = new FormData();
-        postData.append("title", title);
-        postData.append("content", content);
-        postData.append('image', image, title);
-        this.http.post<{ message: string, post: Post }>('http://localhost:3000/api/posts', postData)
-            // Alternate way to send the transformedPost with an id to .subscriber().    
-            // .pipe(map((postMongData) => {
-            //     console.log(postMongData.message);
-            //     const post = postMongData.post;
-            //     return {
-            //         title: post.title,
-            //         content: post.content,
-            //         id: post._id
-            //     }
-            // }))
-            .subscribe((responseData) => {
-                //** Imp Note: The below code to emit change post event is commented off since we navigate 
-                // back to the root where we reload the posts by calling the getPosts() method from the ngOnInit() method of the post-list.component */
-
-                //console.log(postMongData.message);
-                // const post: Post = {
-                //     id: responseData.post.id,
-                //     title: title,
-                //     content: content,
-                //     imagePath: responseData.post.imagePath
-                // }
-                // this.posts.push(post);
-                // this.postsUpdated.next([...this.posts]);
-                this.router.navigate(["/"]);
-            });
-
     }
 
     deletePost(postId: string) {
